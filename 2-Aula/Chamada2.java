fca6cf6ea1ce04c821b59dc356565110ce7d15a9
vcdm100 <vitaomoura100@gmail.com>
2023-02-16 20:46:08 -0300

    Exemplo de Aula 02

---
+++ 2-Aula/Chamada2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Chamada2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, informe o seu NOME COMPLETO?");
+        String nomeCompleto = scanner.next();
+        System.out.println("Seu nome é " + nomeCompleto);
+
+        System.out.println("Por favor, informe a sua IDADE?");
+        int idade = scanner.nextInt();
+        System.out.println("Sua idade é " + idade);
+        
+        Aluno aluno = new Aluno(nomeCompleto, idade);
+    }
+}
