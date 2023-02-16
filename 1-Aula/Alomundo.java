390fa7e0cae18b4aa056954d115f9e2d4cadbf75
vcdm100 <vitaomoura100@gmail.com>
2023-02-16 20:43:10 -0300

    Exemplo de Aula 01

--- 1-Aula/Alomundo.java
+++ 1-Aula/Alomundo.java
@@ -3,14 +3,19 @@
 */
 class AloMundo {
     public static void main(String[] args) {
+
         System.out.println("Alo Mundo");
+
         mostrarNaTela();
+
     }
 
     public static void mostrarNaTela() {
+
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
+        
     }
     
 }
