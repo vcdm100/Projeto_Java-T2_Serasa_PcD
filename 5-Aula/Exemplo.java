6a988f7d73d4c1a8bd8b47c888e68c8d3be22552
vcdm100 <vitaomoura100@gmail.com>
2023-02-27 22:05:39 -0300

    Exemplo de Aula 05

---
+++ 5-Aula/Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+    String atributoCorCarro = "vermelho";
+
+    public void metodoRetornaCor() {
+        System.out.println("A cor do carro é " + atributoCorCarro);
+    }
+    
+}
---
+++ 5-Aula/Main.java
@@ -0,0 +1,98 @@
+/*
+ * class Pessoa {
+ * 
+    private String Nome;
+    private int idade;
+    public String NomeSocial;
+
+    public String retornarNomeParaAmigo() {
+        return Nome;
+    }
+
+}
+
+ * public class Main {
+
+    public static void main (String[] args) {
+        
+        Pessoa pessoa = new Pessoa();
+        pessoa.Nome = "Victor";
+        pessoa.idade = 24;
+
+    }
+
+}
+ */
+
+
+/*class Aluno {
+    public String Nome;
+    private float nota;
+    boolean aprovado;
+
+    public Aluno (String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setNota (float nota) {
+        this.nota = nota;
+    }
+
+    public float getNota () {
+        return this.nota;
+    }
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Aluno aluno = new Aluno("Victor");
+        aluno.setNota(10);
+
+        System.out.println(aluno.getNota());
+    }
+
+}*/
+
+
+class Produto {
+    String codigo;
+    String nome;
+    double preco;
+    int quantidade;
+    private int desconto = 30;
+
+    public Produto(String codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+
+    }
+
+    public double valorComDesconto() {
+        return preco * quantidade * (1 - desconto / 100);
+    }
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Produto novoProdutoCoca = new Produto("001", "Coca-Cola", 5.00);
+        Produto novoProdutoArroz = new Produto("002", "Arroz", 10.00);
+    }
+
+    /*public static void main(String[] args) {
+
+        Array<Produto> produtos = new Array<Produto>();
+
+        produtos.append(
+            new Produto("001", "Coca-Cola", 5.00),
+            new Produto("002", "Arroz", 10.00),
+        )
+        
+    }*/
+
+
+
+}
